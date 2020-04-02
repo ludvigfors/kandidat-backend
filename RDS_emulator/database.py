e28a098327714c8c1e43a7b1e2c7521e69e4c89b
Ludvig Fors <ludfo119@student.liu.se>
2020-04-02 21:31:04 +0200

    Changed "echo" to False

--- RDS_emulator/database.py
+++ RDS_emulator/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///images.db', echo=True)
+engine = create_engine('sqlite:///images.db', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
